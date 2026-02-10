29a7e1db6d1d07c61edda407710598959be7abe9
white871 <white871@purdue.edu>
2026-02-10 15:19:33 -0500

    Added Hall Effect Test Script

---
+++ Raspberry Pi Files/TestHallEffect.py
@@ -0,0 +1,21 @@
+import time
+import gpiozero
+import threading
+
+hallEffect = []
+for pin in [22, 28, 26, 24, 21, 23, 27, 29]:
+    hallEffect.append(gpiozero.DigitalInputDevice(f"BOARD{pin}", pull_up = True))
+
+def hallEffectRefresh():
+    global hallEffect
+    i = 0
+    for sensor in hallEffect:
+        print(str(sensor.value), end=' ')
+        i += 1
+    print('', end='\r')
+    threading.Timer(0.1, hallEffectRefresh).start()
+
+print("\n=============================")
+print("LEAP Hall Effect Testing Tool")
+print("=============================\n")
+hallEffectRefresh()

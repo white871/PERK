3ce79e184e085eb83f79e976f0fd529bee0c28c6
white871 <white871@purdue.edu>
2026-05-06 13:18:29 -0400

    Updated bootup file too

--- RPI_Files/bootup.sh
+++ RPI_Files/bootup.sh
@@ -1,7 +1,4 @@
 bash setup_tlv.sh
-
-#( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.2s ; kill -9 $pid
-#( speaker-test -t sine -f 1500 )& pid=$! ; sleep 0.2s ; kill -9 $pid
-
+bash speaker.sh
 pyperk/bin/python transliteration.py
 
